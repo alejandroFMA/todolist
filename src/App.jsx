6ec7cbe80583c42af294a79282d68fab53f46ef7
Alex Marquez <alexfmarquez@gmail.com>
2023-11-29 09:20:01 +0100

    add/button dissapear after x seconds

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,19 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import reactLogo from './assets/monkey-face.svg'
 import './App.css'
 import Item from './Item'
-
+import customData  from './customData.json'
 import { v4 as uuidv4 } from 'uuid';
 uuidv4(); // ⇨ '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d'
 
 function App() {
   
-  const initialData = [{
-    task: "Finish react exercise"
+  
 
-  }]
-
-  const [data, setData] = useState({}); //Deseo actual
-  const [list, setList] = useState(initialData); //[{}]lista deseos
+  const [data, setData] = useState( {task:"", url:""}); //Deseo actual
+  const [list, setList] = useState([customData]); //[{}]lista deseos
   const [show, setShow] = useState(false);
-
+  const [message, setMessage] = useState("")
 
   const paintTasks = () => {
    
@@ -35,13 +31,13 @@ function App() {
   };
 
   const clearTasks = () =>{
-
+    setData({});
     setList([]); 
   }
 
   const resetTasks = () =>{
-
-    setList(initialData); 
+    setData({});
+    setList([customData]); 
   }
 
 
@@ -52,10 +48,16 @@ function App() {
 
 
  
-    const handleChange = (e) => {
-      if (e.target.value.length > 5)
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setData(prevData => ({ ...prevData, [name]: value }));
+      if (e.target.value.length > 5) {
         setShow(true);
-      else
+        setTimeout(() => {
+          setData({ task: "", url: "" });
+          setShow(false)  
+        }, 5000)
+       } else
         setShow(false)
   };
 
@@ -65,22 +67,37 @@ function App() {
     const task = e.target.task.value;
     const url = e.target.url.value;
    
-    const todo = { task, url };
-    setData({ task, url });
-    setList([...list, todo]);
-    alert(`Task: ${task} registred`);
-    e.target.task.value=""
-    e.target.url.value=""
-    setShow(false)
+    const toDo = { task, url };
+
+    const confirmated = confirm(
+      `Do you want to add this task? task:${task} url:${url}`
+    );
+
+    if (confirmated) {
+      setData({ toDo }); //genera deseo {}
+      setList([...list, toDo]); // [{}, {}] lista deseos
+      alert("Task registered");
+      setData({ task, url });
+      setList([...list, toDo]);
+      e.target.task.value=""
+      e.target.url.value=""
+      setShow(false)
+      setMessage(`${task} was assed to your list`)
+      setTimeout(() => {
+        setMessage("")
+      }, 5000);
+  
+  
+    }
+
+    
+
   };
 
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
@@ -93,12 +110,13 @@ function App() {
         <label htmlFor="name">Image</label><br />
         <input type="url" name="url" 
         onChange = {handleChange}/><br />
-        {show && <button> SEND </button>} 
-        <button onClick={clearTasks}> CLEAR </button>
-        <button onClick={resetTasks}> RESET </button>
+        {show && <button> SEND </button>}      
       </form>
+      <button onClick={clearTasks}> CLEAR </button>
+      <button onClick={resetTasks}> RESET </button>
      
       <h1>To Do List</h1>
+      {message && <div>{message}</div>}
       <section className="itemcontainer">{paintTasks()}</section>
 
     </>
